c48003541bd2f4140974ccb79d349dcbf1f3d4d8
Bartleby <bartleby2718@gmail.com>
2019-07-23 01:07:04 +0900

    Visualize the final rankings with horizontal bar chart close #1

--- output_elo_ratings.py
+++ output_elo_ratings.py
@@ -1,15 +1,18 @@
 from typing import List
 import datetime
+import matplotlib.pyplot as plt
+import numpy as np
 
 K_FACTOR: int = 32  # https://en.wikipedia.org/wiki/Elo_rating_system#Mathematical_details
 INPUT_FILE_NAME: str = 'rummikub.csv'
 OUTPUT_FILE_NAME: str = 'elo_ratings.csv'
+INITIAL_ELO_RATING: float = 1000
 
 
 class Player:
     def __init__(self, name: str = ''):
         self.name: str = name
-        self.rating: float = 1000  # initial ELO rating
+        self.rating: float = INITIAL_ELO_RATING
 
 
 class GameRecord:
@@ -68,10 +71,44 @@ def main():
 
     # output result
     with open(OUTPUT_FILE_NAME, 'w', encoding='utf8') as file:
+        file.write('Player,Elo Rating\n')
         players.sort(key=lambda x: float(x.rating), reverse=True)
         for player in players:
             file.write('{},{:.2f}\n'.format(player.name, player.rating))
 
 
+def visualize_rankings(rankings_file_name: str, save_image_file=True):
+    # Fixing random state for reproducibility
+    np.random.seed(19680801)
+
+    plt.rcdefaults()
+    fig, ax = plt.subplots()
+
+    players: List[str] = []
+    ratings: List[float] = []
+    with open(rankings_file_name) as file:
+        file.readline()  # skip the header
+        for line in file:
+            columns = line.split(',')
+            player: str = columns[0]
+            rating: float = float(columns[1])
+            players.append(player)
+            ratings.append(rating)
+    members = np.arange(len(players))
+
+    ax.barh(members, ratings, align='center')
+    ax.set_yticks(members)
+    ax.set_yticklabels(players)
+    ax.invert_yaxis()  # labels read top-to-bottom
+    ax.set_title('Bapul Team Rummikub Rankings ')
+    ax.set_xlabel('Elo Ratings')
+    plt.axvline(x=INITIAL_ELO_RATING, color='gray', linewidth=.5)
+
+    if save_image_file:
+        plt.savefig('Rankings.png')
+    plt.show()
+
+
 if __name__ == '__main__':
     main()
+    visualize_rankings(OUTPUT_FILE_NAME)
